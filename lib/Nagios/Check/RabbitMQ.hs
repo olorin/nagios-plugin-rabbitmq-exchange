6ecc94cdc1f5d3c8f5571ae0b11e16853164fddc
Sharif Olorin <sio@tesser.org>
2015-01-23 02:19:10 +0000

    Import code from anchor/haskell-nagios-checks

---
+++ lib/Nagios/Check/RabbitMQ.hs
@@ -0,0 +1,7 @@
+module Nagios.Check.RabbitMQ
+  ( module Nagios.Check.RabbitMQ.Types
+  , module Nagios.Check.RabbitMQ.Options
+  ) where
+
+import           Nagios.Check.RabbitMQ.Options
+import           Nagios.Check.RabbitMQ.Types
